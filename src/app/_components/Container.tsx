653130a9990502a3d3b82294b877aafc4265a5de
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-21 16:38:26 -0300

    feat: ipad-curso added and configured, Container and Header components created

---
+++ src/app/_components/Container.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { IPadCursorProvider, useIPadCursor } from "ipad-cursor/react";
+import type { IpadCursorConfig } from "ipad-cursor";
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+const Container = ({ children }: ContainerProps) => {
+  const config: IpadCursorConfig = {};
+  useIPadCursor();
+
+  return (
+    <main className="container mt-12 px-5 md:px-20 lg:px-56 xl:px80 flex flex-col gap-3">
+      <IPadCursorProvider config={config}>{children}</IPadCursorProvider>
+    </main>
+  );
+};
+
+export default Container;
---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>oi</div>;
+};
+
+export default Header;
