266ff7bbaf84d6feeb390c19941cf35e1dd786b1
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-25 22:50:46 -0300

    refactor: footer and link component improved

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer className="flex flex-col md:flex-row sm:flex-col items-center justify-between my-8 md:my-14">
       <div className="mt-9 sm:mb-3">
         <small className="text-sm font-normal leading-none">
-          2024 &copy;
-          <Links olink="https://github.com/imafancydev">imafancydev</Links> .
+          2022-present &copy;
+          <Links olink="https://github.com/imafancydev">imafancydev</Links>.
         </small>
       </div>
     </footer>
