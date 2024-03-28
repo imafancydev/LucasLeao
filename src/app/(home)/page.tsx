4d926d1d329b01789e30447bce29d8e44abb4dd9
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-27 21:43:48 -0300

    feat: content updated at mainsection

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,16 +8,26 @@ const getWakaTimeInfoData = async () => {
 const Home = async () => {
   const userWakaTimeData = await getWakaTimeInfoData();
   return (
-    <MainSection className="text-lg">
+    <MainSection className="text-2xl">
       <div className="flex flex-col items-center font-bold italic sm:flex-row">
         <code
-          className="ml-1 inline-block px-1 leading-none"
+          className="ml-1 inline-block px-1 leading-relaxed"
           data-cursor="block"
           data-cursor-style="--cursor-radius: 50%;--cursor-scale: 1.1;--cursor-blur-duration: 0.7s;border-radius: 6px"
         >
           &#60;coding&nbsp;/&#62;
         </code>
-        <p>for fun in free hours {userWakaTimeData.data.text} </p>
+        <p>for fun in free hours</p>
+      </div>
+      <div className="flex flex-col items-center font-bold italic sm:flex-row">
+        <span data-cursor="text">{userWakaTimeData.data.text} of</span>
+        <code
+          className="coding ml-1 leading-relaxed"
+          data-cursor="block"
+          data-cursor-style="--cursor-radius: 50%;--cursor-scale: 1.1;--cursor-blur-duration: 0.7s;border-radius: 6px"
+        >
+          &#60;coding&nbsp;/&#62;
+        </code>
       </div>
     </MainSection>
   );
