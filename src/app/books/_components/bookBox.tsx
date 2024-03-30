6d45834b3ed17a3214aaba50cbc7c45a2143d5d3
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-30 18:49:51 -0300

    feat: Books page finalized and ready to add more books

--- src/app/books/_components/bookBox.tsx
+++ src/app/books/_components/bookBox.tsx
@@ -1,3 +1,8 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { TBooksData } from "@/types/BooksData";
 import Image from "next/image";
 
@@ -9,16 +14,23 @@ export const BookBox = ({ book }: BookProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded-xl border p-2 shadow">
       <div className="flex flex-col space-y-1 p-6">
-        <h3 className="line-clamp-1 text-center text-xl font-bold italic tracking-tight">
-          {book.name}
-        </h3>
+        <Popover>
+          <PopoverTrigger>
+            <h3 className="line-clamp-1 text-center text-xl font-bold italic tracking-tight">
+              {book.name}
+            </h3>
+          </PopoverTrigger>
+          <PopoverContent>
+            <a>{book.name}</a>
+          </PopoverContent>
+        </Popover>
       </div>
-      <div className="h-max-[200px] flex h-full items-center justify-center rounded-md p-6">
+      <div className="h-max-[200px] flex h-full items-center justify-center rounded-md p-5">
         <Image
           src={book.icon[0]}
           width={200}
           height={200}
-          className="max-h-[250px] max-w-[200px] rounded-md bg-center"
+          className="max-h-[150px] max-w-[180px] rounded-md bg-cover bg-center"
           style={{ objectFit: "contain" }}
           alt={book.name}
         />
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -5,7 +5,13 @@ import { portfolioBooks } from "@/constants/books";
 const MyBooks = () => {
   return (
     <MainSection className="text-2xl">
-      <div className="flex flex-col items-center gap-4 sm:flex-row">
+      <div className="mb-7 flex flex-col items-center justify-center gap-5 font-bold italic">
+        <h1>Livros</h1>
+        <p className="text-base">
+          livros que estão me ajudando a evoluir na carreira
+        </p>
+      </div>
+      <div className="grid w-full grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
         {portfolioBooks.map((book) => (
           <BookBox key={book.id} book={book} />
         ))}
