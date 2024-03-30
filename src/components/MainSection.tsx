18bdfdce6441b646927379e9e55a620f40da75c0
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-30 16:22:01 -0300

    fix: ensure MainSection get ipad-cursor when switch pages

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { twMerge } from "tailwind-merge";
+import { IPadCursorProvider, useIPadCursor } from "ipad-cursor/react";
+import type { IpadCursorConfig } from "ipad-cursor";
 
 interface MainSectionProps {
   className: string;
@@ -6,7 +10,23 @@ interface MainSectionProps {
 }
 
 const MainSection = ({ className, children }: MainSectionProps) => {
-  return <main className={twMerge("my-8", className)}>{children}</main>;
+  const config: IpadCursorConfig = {
+    normalStyle: {
+      backdropBlur: "20px",
+    },
+    blockStyle: {
+      backdropBlur: "0px",
+      durationBackdropFilter: "0.5s",
+    },
+    enableAutoTextCursor: true,
+  };
+
+  useIPadCursor();
+  return (
+    <IPadCursorProvider config={config}>
+      <main className={twMerge("my-8", className)}>{children}</main>
+    </IPadCursorProvider>
+  );
 };
 
 export default MainSection;
