b6fa50ef1df266a1e3c8b262fd43188b23a8a192
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-04-01 18:41:09 -0300

    feat: constant Books updated

--- src/constants/books.ts
+++ src/constants/books.ts
@@ -22,4 +22,32 @@ export const portfolioBooks: TBooksData[] = [
       "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1510834068i/36605373.jpg",
     ],
   },
+  {
+    id: 4,
+    name: "Cientista da Computação Autodidata: Guia de Estruturas de Dados e Algoritmos Para o Iniciante",
+    icon: [
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1679436782i/123377042.jpg",
+    ],
+  },
+  {
+    id: 5,
+    name: "Estruturas de Dados e Algoritmos com JavaScript: Escreva um Código JavaScript Complexo e Eficaz Usando a Mais Recente ECMAScript",
+    icon: [
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1583109799i/51981232.jpg",
+    ],
+  },
+  {
+    id: 6,
+    name: "Arquitetura Limpa. O Guia do Artesao Para Estrutura e Design de Software",
+    icon: [
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1560436359l/49908812.jpg",
+    ],
+  },
+  {
+    id: 7,
+    name: "Domain-driven design: atacando as complexidades no coração do software",
+    icon: [
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1287493789i/179133.jpg",
+    ],
+  },
 ];
